1680f536bd128d5a4a45515a486d4cc1bed4f0f6
asad149 <asad.arshad.94849@gmail.com>
2020-10-14 00:45:46 +0500

    Successfully Developed Upload Image End Point

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const pusher = require("pusher");
 const Grid = require("gridfs-stream");
 const mongoose = require("mongoose");
 const GridFsStorage = require("multer-gridfs-storage");
+const { rejects } = require("assert");
+const { resolve } = require("path");
 const mongoURI = require("./config/keys").mongoURI;
 
 Grid.mongo = mongoose.mongo;
@@ -45,8 +47,29 @@ conn.once("open", () => {
   gfs.collection("images");
 });
 
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      {
+        const filename = `image${Date.now()}${path.extname(file.originalname)}`;
+        const fileinfo = {
+          filename: filename,
+          bucketName: "images",
+        };
+        resolve(fileinfo);
+      }
+    });
+  },
+});
+
+const upload = multer({ storage });
+
 // Route
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+app.post("/upload/image", upload.single("file"), (req, res) => {
+  res.status(201).send(req.file);
+});
 
 // Listen
 app.listen(port, () => console.log(`App is running on port ${port}`));
